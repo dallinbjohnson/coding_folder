387bd2531c54844dc5ba52dfa203e35e26599ff9
dallinbjohnson <dallin.b.johnson@gmail.com>
2020-04-12 14:35:31 -0600

    Add Regex password challenge.

---
+++ python/challenge/codeWars/RegexPasswordValidation.py
@@ -0,0 +1,39 @@
+"""
+You need to write regex that will validate a password to make sure it meets the following criteria:
+
+At least six characters long
+contains a lowercase letter
+contains an uppercase letter
+contains a number
+Valid passwords will only be alphanumeric characters.
+"""
+
+regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*[\\!@#\$\%\^\&\*\(\)\{\}\[\],\.\?\"\:\|\<\>\-\+\=\`\~\/_\s])[A-Za-z\d]{6,}$"
+
+#
+# Other options
+#
+
+from re import compile, VERBOSE
+
+regex = compile("""
+^              # begin word
+(?=.*?[a-z])   # at least one lowercase letter
+(?=.*?[A-Z])   # at least one uppercase letter
+(?=.*?[0-9])   # at least one number
+[A-Za-z\d]     # only alphanumeric
+{6,}           # at least 6 characters long
+$              # end word
+""", VERBOSE)
+
+
+import re
+regex = re.compile(r"""
+    (?=^[a-zA-Z0-9]{6,}$)
+    (?=\w*[a-z])
+    (?=\w*[A-Z])
+    (?=\w*\d)
+""", re.X)
+
+
+regex='(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])^[^\W_]{6,}$'
